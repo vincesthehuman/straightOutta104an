979466664841ad2d2811716f2083cd4b5f4cc21c
Maddis87 <madeleneamberman@gmail.com>
2017-11-09 14:05:46 +0100

    header down

--- straight-outta-104an/src/components/layout/header/Header.js
+++ straight-outta-104an/src/components/layout/header/Header.js
@@ -8,7 +8,7 @@ import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
+//import MenuIcon from 'material-ui-icons/Menu';
 
 const styles = theme => ({
   root: {
@@ -31,7 +31,7 @@ function ButtonAppBar(props) {
       <AppBar position="static">
         <Toolbar>
           <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
-            <MenuIcon />
+            
           </IconButton>
           <Typography type="title" color="inherit" className={classes.flex}>
             Straight outta 104an!
--- straight-outta-104an/src/components/pages/main/Main.js
+++ straight-outta-104an/src/components/pages/main/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Header from '../../layout/header/Header.js'
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-      
+        <Header/>
       </div>
     )
   }
