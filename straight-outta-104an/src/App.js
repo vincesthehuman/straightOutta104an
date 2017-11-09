b1cbd7dd24e8ff749a22482a66ef518f5c24e4da
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 11:47:17 +0100

    added folders for components

--- straight-outta-104an/src/App.js
+++ straight-outta-104an/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
+import { withStyles } from 'material-ui/styles';
+import Button from 'material-ui/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Button raised></Button>
       </div>
     )
   }
