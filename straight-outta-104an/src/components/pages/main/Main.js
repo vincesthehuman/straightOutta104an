1aa9cd68c85afed292da5560423e42218a8db90f
Rasmus Falk <rf222fu@student.lnu.se>
2017-11-09 13:00:56 +0100

    Modified card

--- straight-outta-104an/src/components/pages/main/Main.js
+++ straight-outta-104an/src/components/pages/main/Main.js
@@ -4,11 +4,6 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-<<<<<<< HEAD
-
-=======
-      
->>>>>>> 62757cdf9d177aa7b1a32605f7f7148cbbfa0f9e
       </div>
     )
   }
