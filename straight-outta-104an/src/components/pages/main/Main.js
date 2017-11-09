539fc3382db8da8fc6e3bd25e530ccc7d6bad88e
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 16:17:40 +0100

    Drawer works like it should

--- straight-outta-104an/src/components/pages/main/Main.js
+++ straight-outta-104an/src/components/pages/main/Main.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import Header from '../../layout/header/Header.js'
-import MainDrawer from '../../layout/drawer/MainDrawer'
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
         <Header/>
-        <MainDrawer />
       </div>
     )
   }
