36428ebcd829eab14721c6c17b788cba27036e80
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 12:41:41 +0100

    added a drawer

---
+++ straight-outta-104an/src/components/layout/drawer/Drawer.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Drawer from 'material-ui/Drawer'
+import Button from 'material-ui/Button'
+import List from 'material-ui/List'
+import Divider from 'material-ui/Divider'
+import { mailFolderListItems, otherMailFolderListItems } from './tileData'
+
+const styles = {
+  list: {
+    width: 250
+  },
+  listFull: {
+    width: 'auto'
+  }
+}
+
+class TemporaryDrawer extends React.Component {
+  state = {
+    left: false
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open
+    })
+  }
+
+  render() {
+    const { classes } = this.props
+
+    const sideList = (
+      <div className={classes.list}>
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </div>
+    )
+
+    const fullList = (
+      <div className={classes.listFull}>
+        <List>{mailFolderListItems}</List>
+        <Divider />
+        <List>{otherMailFolderListItems}</List>
+      </div>
+    )
+
+    return (
+      <div>
+        <Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
+        <Drawer
+          open={this.state.left}
+          onRequestClose={this.toggleDrawer('left', false)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('left', false)}
+            onKeyDown={this.toggleDrawer('left', false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
+        <Drawer
+          anchor="top"
+          open={this.state.top}
+          onRequestClose={this.toggleDrawer('top', false)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('top', false)}
+            onKeyDown={this.toggleDrawer('top', false)}
+          >
+            {fullList}
+          </div>
+        </Drawer>
+        <Drawer
+          anchor="bottom"
+          open={this.state.bottom}
+          onRequestClose={this.toggleDrawer('bottom', false)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('bottom', false)}
+            onKeyDown={this.toggleDrawer('bottom', false)}
+          >
+            {fullList}
+          </div>
+        </Drawer>
+        <Drawer
+          anchor="right"
+          open={this.state.right}
+          onRequestClose={this.toggleDrawer('right', false)}
+        >
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('right', false)}
+            onKeyDown={this.toggleDrawer('right', false)}
+          >
+            {sideList}
+          </div>
+        </Drawer>
+      </div>
+    )
+  }
+}
+
+TemporaryDrawer.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Drawer)
