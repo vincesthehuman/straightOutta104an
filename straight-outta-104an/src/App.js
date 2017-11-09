a33690ac19d1e5db625a8f44b8b9f2cc2811ad62
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 10:40:45 +0100

    pushing picture

--- straight-outta-104an/src/App.js
+++ straight-outta-104an/src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Straight outta 104an</h1>
-        </header>
-        <p className="App-intro">
-          Here, news will be shown.
-        </p>
+        
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
