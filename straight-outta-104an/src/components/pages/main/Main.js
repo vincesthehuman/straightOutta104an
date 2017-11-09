c9fe79f39048f76b64370f4ba1154b372ce64d00
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 12:45:49 +0100

    added main

---
+++ straight-outta-104an/src/components/pages/main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+
+      </div>
+    )
+  }
+}
+
+export default Main
