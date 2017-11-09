3a8b9a13fcb61d792ddf8749f3f32342fc89e810
vincesthehuman <wr222ae@student.lnu.se>
2017-11-09 14:03:41 +0100

    Drawer is divided into smaller components

--- straight-outta-104an/src/components/pages/main/Main.js
+++ straight-outta-104an/src/components/pages/main/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import MainDrawer from '../../layout/drawer/MainDrawer'
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-      
+        <MainDrawer />
       </div>
     )
   }
